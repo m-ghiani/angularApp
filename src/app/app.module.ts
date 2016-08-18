75925a4904ead623c9a7fddef7ac9be722029734
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-18 11:36:55 +0200

    fatte modifiche che hanno fatto fallire i test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HTTP_PROVIDERS,HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,10 @@ import { UserComponent } from './user/user.component';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HTTP_PROVIDERS],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
