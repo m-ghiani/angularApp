bb4c1991b91b3544b519337a3947ff0ea5bfe323
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-18 18:37:30 +0200

    boh

--- src/app/machinery/machinery.service.ts
+++ src/app/machinery/machinery.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
-
+import { Http,Headers,Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Machinery } from './machinery.model';
+import { Message } from './message.model';
 @Injectable()
 export class MachineryService {
 
-  constructor() { }
+  // URL where is the REST api for machine 
+  private machineURL : string = "http://seserver.noip.me:8080/rest/";
+  constructor(private http : Http) { }
 
+  // @method for receiving all machines in the database
+  getAll() : Observable<Message>{
+    let machines = this.http.get(`${this.machineURL}machines`,{headers:this.getHeaders()})
+                    .map((received_machines:Response) => received_machines.json());
+    return machines;
+  }
+  // @method for receiving one machines in the database by id
+  getById(id:number) : Observable<Message>{
+    let machines = this.http.get(`${this.machineURL}machines/${id}`,{headers:this.getHeaders()})
+                    .map((received_machines:Response) => received_machines.json());
+    return machines;
+  }
+  // @method for adding one machine in the databse by id
+  postmachine(machine:Machinery) : Observable<Message>{
+    let machines = this.http.post(`${this.machineURL}machines`,JSON.parse(JSON.stringify(machine)),{headers:this.getHeaders()})
+                    .map((received_machines:Response) => received_machines.json());
+    return machines;
+  }
+  // @method for modify one machine in the databse by id
+  putmachine(id:number,machine:Machinery) : Observable<Message> {
+    let machines = this.http.put(`${this.machineURL}machines/${id}`,JSON.parse(JSON.stringify(machine)),{headers:this.getHeaders()})
+                    .map((received_machines:Response) => received_machines.json());
+    return machines;
+  }
+
+  // @method for deleting one machine from the database by id
+  deletemachine(id:number) : Observable<Message> {
+    let machines = this.http.delete(`${this.machineURL}machines/${id}`,{headers:this.getHeaders()})
+                    .map((received_machines:Response) => received_machines.json());
+    return machines;
+  }
+
+  // @method to create headers for this http service
+  private getHeaders(){
+        let headers = new Headers();
+        headers.append('Accept','application/json');
+        return headers;
+    }
 }
