8f57b6067955eabad9572330de3983b5569dfb34
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-18 11:44:40 +0200

    eliminato bootstrap e inserito service in user.component.spec

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -4,10 +4,12 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('Component: User', () => {
   it('should create an instance', () => {
-    let component = new UserComponent();
+    let service : UserService;
+    let component = new UserComponent(service);
     
   });
 });
