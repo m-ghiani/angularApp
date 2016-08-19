137fa8a75d72943c11a9a7d93362a80e84bc655e
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-19 17:44:23 +0200

    aggiunto coordinate a machinery

--- src/app/machinery/machinery.model.ts
+++ src/app/machinery/machinery.model.ts
@@ -2,7 +2,7 @@ import { Coordinate } from './coordinate.model';
 
 export class Machinery {
     _id:number;
-    gps:any;
+    gps:Coordinate;
     state:string;
     id_prod:Array<number>
     type:Array<string>
