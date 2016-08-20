d61a3f2f0b2b9a8f97e3f7f2ee1ec3e176dbf93c
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-20 07:27:22 +0200

    modifiche ai test

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -16,7 +16,47 @@ describe('Service: User', () => {
         service.getAll()
             .subscribe(data => {
                   console.log("Service: User getAll() method testing result: " + data.error);
-                  expect(data.error).toBe(false);
+                  expect(data).toEqual({
+  "error": false,
+  "message": [
+    {
+      "_id": 1,
+      "email": "marco.mameli@gmail.com",
+      "password": "1234",
+      "type": "admin",
+      "id_mac": [
+        1,
+        2,
+        3,
+        4
+      ]
+    },
+    {
+      "_id": 2,
+      "email": "andrea.cuna@gmail.com",
+      "password": "abcd",
+      "type": "admin",
+      "id_mac": [
+        1,
+        2,
+        3,
+        4
+      ]
+    },
+    {
+      "_id": 3,
+      "email": "luigi.pirandello@gmail.com",
+      "password": "unonessunocentomila",
+      "type": "registered",
+      "id_mac": [
+        1,
+        2,
+        3,
+        4
+      ]
+    }
+  ]
+});
             },
             err => console.log(err));
       }));
@@ -26,7 +66,23 @@ describe('Service: User', () => {
         service.getByEmail("marco.mameli@gmail.com")
             .subscribe((data:Message) => {
                   console.log("Service: User getByEmail() method testing result: " + data.error);
-                  expect(data.error).toBe(false);
+                  expect(data).toEqual({
+  "error": false,
+  "message": [
+    {
+      "_id": 1,
+      "email": "marco.mameli@gmail.com",
+      "password": "1234",
+      "type": "admin",
+      "id_mac": [
+        1,
+        2,
+        3,
+        4
+      ]
+    }
+  ]
+});
             },
             err => console.log(err));
       }));
