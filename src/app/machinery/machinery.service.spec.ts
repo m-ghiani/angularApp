60cf4d330fcfbe13b578ceb67403084c83d14273
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-20 09:40:19 +0200

    aggiunto test secondario in machine

--- src/app/machinery/machinery.service.spec.ts
+++ src/app/machinery/machinery.service.spec.ts
@@ -15,6 +15,7 @@ describe('Service: Machinery', () => {
       ( service:MachineryService) => {
         service.getAll()
             .subscribe(data => {
+                  console.log("getAllMachines result:" + data);
                   expect(data).toEqual({
   "error": false,
   "message": [
