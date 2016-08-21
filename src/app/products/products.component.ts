b02aab8165733e6b1b1756dba6db13377c7f5b0d
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-21 16:59:50 +0200

    eliminazione angular-ng2-file-upload

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './products.service';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle } from '@angular/common';
-import { FILE_UPLOAD_DIRECTIVES, FileUploader } from 'ng2-file-upload';
 
 
 @Component({
   selector: 'app-products',
   templateUrl: 'products.component.html',
   styleUrls: ['products.component.css'],
-  directives: [FILE_UPLOAD_DIRECTIVES, CORE_DIRECTIVES,FORM_DIRECTIVES],
   providers: [ProductsService]
 })
 export class ProductsComponent implements OnInit {
 
-  public uploader:FileUploader = new FileUploader({url: "http://seserver.noip.me:8080/rest/images/"});
-  public hasBaseDropZoneOver:boolean = false;
-  public hasAnotherDropZoneOver:boolean = false;
   id_product : number; // questo sarà sia il nome del file immagine che l'id del prodotto
   image : string;
   constructor(private productservice : ProductsService) { }
@@ -24,14 +19,6 @@ export class ProductsComponent implements OnInit {
 
   // visualizzazione immagine
    this.image = this.productservice.getResourceImage("undefined-1471755398382.jpeg");
-  }
-
-  public fileOverBase(e:any):void {
-    this.hasBaseDropZoneOver = e;
-  }
-
-  public fileOverAnother(e:any):void {
-    this.hasAnotherDropZoneOver = e;
   }
 }
 
