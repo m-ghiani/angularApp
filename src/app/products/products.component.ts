94f5682dd70d75ebd55fb69651cbbc2152713049
Marco Mameli <mameli.1.marco@gmail.com>
2016-08-21 17:52:29 +0200

    comopnents

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './products.service';
-import { CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle } from '@angular/common';
-
 
 @Component({
   selector: 'app-products',
@@ -13,12 +11,19 @@ export class ProductsComponent implements OnInit {
 
   id_product : number; // questo sarà sia il nome del file immagine che l'id del prodotto
   image : string;
-  constructor(private productservice : ProductsService) { }
+  submitted : boolean;
+  constructor(private productservice : ProductsService) {
+    
+   }
 
   ngOnInit() {
-
   // visualizzazione immagine
    this.image = this.productservice.getResourceImage("undefined-1471755398382.jpeg");
+   this.id_product = 1;
+   this.onsubmit();
+  }
+  onsubmit(){
+    console.log("Effettuo il submit");
   }
 }
 
